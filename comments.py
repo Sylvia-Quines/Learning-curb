d872c6f32c4e615f4041e73ffd682134cc56ac4f
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-10 22:43:29 +0100

    Comments explain the codes and make them more readable

---
+++ comments.py
@@ -0,0 +1,16 @@
+print ('She is my wife')
+# This will not be printed
+
+# Assigned values to x and y and printed them out
+x = 65
+y =120
+print(x)
+print(y)
+
+# MULTILINE COMMENTS
+""""
+This ia a comments
+writen in 
+more than one line
+
+"""
