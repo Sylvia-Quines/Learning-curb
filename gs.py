328dd00837a86311dd44d53f688e5d9d9796a3f5
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-10 20:35:03 +0100

    Python synthax can be directly executed in the cmd

--- gs.py
+++ gs.py
@@ -1,1 +1,3 @@
 print('Hello World, this is my first python code!')
+import sys
+print(sys.version)
---
+++ synthax.py
@@ -0,0 +1,15 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+x = 5
+y = 'Ama is a good girl'
+
+#This is a python comment
+print('Hello Darla!')
+
+
+#STATEMENTS
+print('Python is good!')
+
+#Using semicolons to separate multiple statements
+print("My name is Darla."); print('She is pretty!'); print('She just left.')
