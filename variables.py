82fbf75a8140cbbc92a17f3cf535920cbe5609e5
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-12 10:09:27 +0100

    A variable name cannot be any of the python keywords

--- variables.py
+++ variables.py
@@ -1,9 +1,17 @@
 myvar = "John"
+print(myvar)
 my_var = "John"
+print(my_var)
+
+#This is not a correct variable name
 
 _my_var = "John"
+print(_my_var)
 
 myVar = "John"
-
+print(myVar)
 MYVAR = "John"
+print(MYVAR)
+
 myvar2 = "John"
+print(myvar2)
