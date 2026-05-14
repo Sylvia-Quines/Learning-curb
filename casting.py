98c268bfb4b0a265de604e78a96c0324148ec83c
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-14 21:07:23 +0100

    Casting is the conversion of a value from one datatype to another

---
+++ casting.py
@@ -0,0 +1,43 @@
+# Casting - converting a value from one datatype to another
+
+v = int('3')
+y = int(8)
+h = int(3.89)
+b = int(-67)
+
+print(v)
+print(y)
+print(h)
+print(b)
+
+v = float('3')
+y = float(8)
+h = float(3.89)
+b = float(-67)
+
+print(v)
+print(y)
+print(h)
+print(b)
+
+v = str('3')
+y = str(8)
+h = str(3.89)
+b = str(-67)
+
+print(v)
+print(y)
+print(h)
+print(b)
+
+
+# Create an integer
+x = 1
+# Convert to float
+a = float(x)
+# Convert to string
+b = str(x)
+# Print values
+print(a)
+print(b)
+
