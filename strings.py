827490c19e39e7d77b641bd61381599ce6661d2f
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-14 21:39:21 +0100

    Strings are arrays

---
+++ strings.py
@@ -0,0 +1,36 @@
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+print('Hello')
+print("Hello")
+
+# QUOTES INSIDE QUOTES
+print('The pen is Jonny"s')
+print("The pen is Jonny's")
+
+a = 'She is his wife'
+print(a)
+
+
+# MULTILINE STRING
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# STRINGS ARE ARRAYS
+a = ('She is a student')
+print(a[4])
+print(3)
+
+# LOOPING THROUGH A STRING
+for x in 'She is back':
+    print(x)
+    
+for y in 'I am fine':
+    print(y)    
+
+# Geting the length of a string
+g = 'When is my mum coming?'
+print(len(g))
+
