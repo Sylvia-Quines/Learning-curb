fb40c5acc07b004d66ab775cc806f9e55087a9c6
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-18 14:52:49 +0100

    Booleans represent true or false

---
+++ Booleans.py
@@ -0,0 +1,14 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+Price = 100
+Balance = 200
+Age = 23
+if Price >= Balance:
+    print('I will purchase')
+else:
+    print('I will not')     
+   
+    
