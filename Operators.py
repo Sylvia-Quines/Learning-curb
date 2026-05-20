26d48d6f673d1ee28424736a597036db14d99ca7
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-20 22:27:51 +0100

    Operators are used to perform operations on values and variables

--- Operators.py
+++ Operators.py
@@ -44,3 +44,15 @@ print(1 < x and x < 10)
 x = 5
 
 print(x > 0 and x < 10)
+
+
+# Identity Operators 
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
