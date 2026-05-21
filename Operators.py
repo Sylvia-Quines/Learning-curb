268c3500edc0c5d713c35c7efc6566016528fb8c
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-21 22:30:06 +0100

    Operators are used to perform operations on values and variables

--- Operators.py
+++ Operators.py
@@ -106,5 +106,25 @@ if (count := len(numbers)) > 3:
 
 # LOGICAL OPRATORS 
 x = 5
-
 print(x > 0 and x < 10) 
+
+x = 5
+print(x < 2 or x > 10)
+
+x = 5
+print(not(x > 3 and x < 10))
+
+# IDENTITY OPERATORS 
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+
+print(x is not y)
