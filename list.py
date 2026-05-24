e0f22e939085543260640c775d1d2cc65bb2de50
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-24 22:47:44 +0100

    Lists are used to store multiple items in a single variable.

--- list.py
+++ list.py
@@ -12,3 +12,8 @@ list1 = ["abc", 34, True, 40, "male"]
 
 mylist = ["apple", "banana", "cherry"]
 print(type(mylist))
+
+
+# Using the list() constructor to make a List: 
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
