f8d270eb31e0068f42f28dd24bbc855a55bfa469
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-05-26 21:36:49 +0100

    Lists are used to store multiple items in a single variable.

--- list.py
+++ list.py
@@ -65,4 +65,16 @@ print(mylist)
 myColor = ['red', 'indigo', 'pink', 'blue']
 myColor[2] = 'white'       
 print(myColor)
-       
+
+myColor = ['red', 'indigo', 'pink', 'blue']
+myColor[1:3] = ['white', 'black']       
+print(myColor)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)       
