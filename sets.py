90810b234362abf515ecc3c840594d06dbd419ef
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-01 20:54:25 +0100

    Sets are used to store multiple items in a single variable.

---
+++ sets.py
@@ -0,0 +1,9 @@
+mySet = {'food', 'money', 'house', 'shoe', 'bag'}
+print(mySet)
+
+mySet = {'food', 'money', 'bag', 'house', 'shoe', 'bag'} #set cannot have duplicates
+print(mySet)
+
+#True and 1 are considered the same values in set same as false and 0
+thisSet = {True, 'False', 'Another', 1 ,6}
+print(thisSet)
