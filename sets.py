32f1afceca3cc875de7b4f1678cbff6fc1f3fe6e
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-01 21:14:47 +0100

    Sets are used to store multiple items in a single variable.

--- sets.py
+++ sets.py
@@ -32,3 +32,9 @@ print('money' in mySet)
 mySet = {'food', 'money', 'house', 'shoe', 'bag'}
 mySet.add('Car')
 print(mySet)
+
+set1 = {1,2,3,4,5,6}
+set2 = {'Dog', 'Cat', 'Pig'}
+set1.update(set2)
+print(set1)
+
