737ac9f65ac88902ee2cdfd36f9c1d3de83a0fda
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-01 21:04:49 +0100

    Sets are used to store multiple items in a single variable.

--- sets.py
+++ sets.py
@@ -8,3 +8,20 @@ print(mySet)
 thisSet = {True, False, 'Another', 1 ,6}
 print(thisSet)
 
+mySet = {'food', 'money', 'house', 'shoe', 'bag'}
+print(len(mySet))
+
+#SETS CAN BE OF ANY DATA TYPE
+set1 = {1,2,3,4,5,6}
+set2 = {'Dog', 'Cat', 'Pig'}
+print(type(set1))
+
+mySet = {'food', 'money', 'house', 'shoe', 'bag'}
+for x in mySet:
+    print(x)
+    
+mySet = {'food', 'money', 'house', 'shoe', 'bag'}
+i = 0
+while i < len(mySet):
+    print(i)
+    i = i+1   
