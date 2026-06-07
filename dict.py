583675523511a6a29164c640609d9841b1f84a72
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-07 20:39:51 +0100

    Dictionaries are used to store data values in key:value pairs.

--- dict.py
+++ dict.py
@@ -122,3 +122,13 @@ thisdict = {
 }
 mydict = thisdict.copy()
 print(mydict)  
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  
+}
+
+yourdict = dict(thisdict)
+print(yourdict)
