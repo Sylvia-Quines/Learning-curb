0fb63556082dcf83fa21abcbca9af897329579ff
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-09 21:41:24 +0100

    Python supports the usual logical operations from maths.

--- ifelse.py
+++ ifelse.py
@@ -12,4 +12,39 @@ if Age > 50:
 is_legal  = True
 if is_legal:
     print('You can vote.')           
+
+
+q = 23
+x = 66
+if q > x:
+    print("Try again!")
+elif x > q:        
+    print("Finally")
+    
+score = 80
+
+if score >= 100:
+    print('GRADE A')
+elif score >= 75:
+    print("GRADE B")    
+elif score >= 50:
+    print('GRADE C')   
+    
+day = 6
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")             
+elif day == 3:
+    print("Wednesday") 
+elif day == 4:
+    print("Thursday") 
+elif day == 5:
+    print("Friday") 
+elif day == 6:
+    print("Saturday") 
+elif day == 7:
+    print("Sunday")                     
+    
     
