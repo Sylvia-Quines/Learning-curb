b9db68270e7afcc16ab2bd8049fdb57e9b39c439
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-16 19:26:51 +0100

    Python supports the usual logical operations from maths.

--- ifelse.py
+++ ifelse.py
@@ -200,4 +200,19 @@ if age > lenght:
     pass
 
 
-
+day = 5
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday") 
