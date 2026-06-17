7d3df78c665d366eeb585ea13a0c27c9f4a14f5b
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-17 20:51:54 +0100

    With while loop we can execute a set of statements as long as a condition is true.

--- whileloop.py
+++ whileloop.py
@@ -3,4 +3,19 @@ while i < 6:
     print(i)
     i += 1 # this is the update step, without it i stays at 1 forever and you would have an infinite loop
     
+x = 2
+
+while x < 32:
+    print(x)
+    if x == 30:
+        break # loop stops when it gets to 30
+    x += 2  
     
+    
+x = 2
+
+while x < 32:
+    print(x)
+    if x == 30:
+        continue # loop stops when it gets to 30
+    x += 2 
