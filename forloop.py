b5dc54332c9d1eb19ca18fdc3be2d3007bafdfd3
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-06-22 20:33:50 +0100

    For loop is used for iterating over a sequence.

--- forloop.py
+++ forloop.py
@@ -26,3 +26,10 @@ for x in fruits:
   
 for x in range (5):
     print(x)
+    
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
