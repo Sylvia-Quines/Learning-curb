399faedee2e2c8a7adc90d8912a2559924fc68b3
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-07-22 21:30:50 +0100

    Function is a block of code which runs when it is called.

--- functions.py
+++ functions.py
@@ -8,3 +8,9 @@ if time <= 6:
     my_function()
 else:
     print("She was available")
+    
+def fahrenheit_to_celsius(fahrenheit):
+    return(fahrenheit - 32) *5 / 9
+
+
+print(fahrenheit_to_celsius(43))
