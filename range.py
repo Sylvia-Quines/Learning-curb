4f1c5cca6abdd9fe0f9fcece95225c69d215c313
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-08-25 09:53:31 +0100

    The built-in range() function returns an immutable sequence of numbers, commonly used for looping a specific number of times.

---
+++ range.py
@@ -0,0 +1,11 @@
+# The range() function can be called with 1, 2, or 3 arguments, using this syntax:
+#range(start, stop, step). When the argument is one it is the stop argument 
+# and the start argument is automatically 0
+v = range(1, 10, 1)
+x = range(20)
+print(x)
+
+# when it has two values the aarguments are start and stop
+i = range(2, 22)
+
+
