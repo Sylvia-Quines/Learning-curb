74fbfad6145cb33cb73e7b44a597538fb46ed23d
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-08-26 21:08:30 +0100

    The built-in range() function returns an immutable sequence of numbers, commonly used for looping a specific number of times.

--- range.py
+++ range.py
@@ -12,4 +12,24 @@ i = range(2, 22)
 #RAnge isoften usedin for loops
 for i in range(3,34):
     print(i)
+    
+print(list(range(5)))
+print(list(range(1, 6)))
+print(list(range(5, 20, 3)))
+
+#slicing range
+r = range(10)
+print(r[2])
+print(r[:3])
+
+#Membership testing
+
+r = range(0, 10, 2)
+print(6 in r)
+print(7 in r)
+
+#Getting the number of elements in a range
+r = range(0, 10, 2)
+print(len(r))
+
 
