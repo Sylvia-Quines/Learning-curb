a22197b5f4a2479218450ac3f53098747099bb94
Sylvia-Quines <sylviafamiehquines@gmail.com>
2026-08-28 10:53:53 +0100

    Arrays are used to store multiple values in one single variable.

---
+++ Arrays.py
@@ -0,0 +1,14 @@
+Girls = ["Adwoa", "Sandy", "Doreen"]
+
+#Accessing the elements of an array
+u = Girls[1]
+print(u)
+
+#Modifying array
+Girls[2] = "Selena"
+Girls[1] = "Sheila"
+print(Girls)
+
+d = len(Girls) #This gets the length of the array
+print(d)
+
